85746a57dafd0c20fd78538c9aa1f605a06fa240
nicolestella <nicolestella@users.noreply.github.com>
2020-08-24 23:21:25 +1000

    ass1 hd

--- COMP2000_Assignment1/COMP2000_2020-7e914d5af55e7996f2483e3f53132a87d8a4f145/src/Sand.java
+++ COMP2000_Assignment1/COMP2000_2020-7e914d5af55e7996f2483e3f53132a87d8a4f145/src/Sand.java
@@ -4,11 +4,11 @@ public class Sand extends CellType {
 
     public Sand(Cell loc) {
         this.loc = loc;
-        this.random = (int)(Math.random() * 100) + 183;
+        this.random = (int)(Math.random() * 72) + 183;
         this.colour = new Color(255, random, 0);
         this.cellType = "Sand";
 
-        if(this.random >= 262 && this.random <= 283){
+        if(this.random >= 240 && this.random <= 255){
             this.viscosity = 15;
         }
         else {
